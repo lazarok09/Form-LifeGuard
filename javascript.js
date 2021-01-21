eea4558a05b4484ea0e862788c96e25913305a29
lazarok09 <lazarok09@gmail.com>
2021-01-21 01:20:09 -0300

    adding modal

--- javascript.js
+++ javascript.js
@@ -25,4 +25,26 @@ function getData(){
     document.getElementById('formulario_input3').value = LINKEDIN;
     
 }
-    
+// modal function
+function runModal() {
+const NAME = document.getElementById('formulario_input').value;
+const GITHUB = document.getElementById('formulario_input2').value;
+const LINKEDIN = document.getElementById('formulario_input3').value;
+
+if (NAME != '' && GITHUB != '' && LINKEDIN != '') {
+ document.getElementById('modal-body').innerHTML = "Sucesso ao persistir os dados em local storage";
+ document.getElementById('modal-title').innerHTML = "Sucesso";
+ document.getElementById('modal-title').className = "text-success";
+ document.getElementById('modal-footer').innerHTML = "Voltar";
+ document.getElementById('modal-footer').className = "btn-success";
+ $('#modal').modal('show');
+} else {
+    document.getElementById('modal-body').innerHTML = "Existem elementos que não foram inseridos";
+    document.getElementById('modal-title').innerHTML = "ERRO!";
+    document.getElementById('modal-title').className = "text-danger";
+    document.getElementById('modal-footer').innerHTML = "VOLTAR E CORRIGIR";
+    document.getElementById('modal-footer').className = "btn btn-info";
+ $('#modal').modal('show');
+}
+
+}
